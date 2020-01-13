66fa5069a8d2f6c3a6bccaff50131a0bc104b238
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-13 17:37:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,6 @@ def initialize(name, artist, genre)
   @@count+=1
 end
 
-
-
 def self.count
   @@count
 end
@@ -42,4 +40,14 @@ def self.genre_count
     end
     genre_count
 end
+
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist]+=1
+    else
+      artist_count[artist] = 1
+    end
+  end
 end
